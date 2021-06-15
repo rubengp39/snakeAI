8ad9419477cca4038289d1abf8e0962a66626e57
Rubén García Pedrejón <62309228+rubengp39@users.noreply.github.com>
2021-06-15 19:47:57 +0200

    RandomStartDirection

--- snakeHuman/snakeHuman/snakeHuman.py
+++ snakeHuman/snakeHuman/snakeHuman.py
@@ -9,10 +9,10 @@ pygame.init()
 font = pygame.font.SysFont('arial', 20)
 
 class Direction(Enum):
-	RIGHT = 1
-	LEFT = 2
-	UP = 3
-	DOWN = 4
+	RIGHT = 0
+	LEFT = 1
+	UP = 2
+	DOWN = 3
 
 
 Point = namedtuple('Point', 'x, y')
@@ -36,7 +36,7 @@ class SnakeGame:
 		self.display = pygame.display.set_mode((self.w, self.h))
 		self.clock = pygame.time.Clock()
 
-		self.direction = Direction.RIGHT
+		self.direction = random.choice(list(Direction))
 
 		self.head = Point(self.w/2, self.h/2)
 		self.snake = [self.head, Point(self.head.x-BLOCK_SIZE, self.head.y), Point(self.head.x-(2*BLOCK_SIZE), self.head.y)]
